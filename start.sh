5f6e077c136a1f6d22f20859c78812c4d08a5c4b
Stanislav Slavin <slavin.stas@gmail.com>
2026-03-14 00:29:25 +0300

    Add outbound HTTP proxy and SOCKS5 server to Tailscale

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@ set -e
 
 if [ -n "$TAILSCALE_AUTHKEY" ]; then
     echo "Starting Tailscale..."
-    tailscaled --state=/var/lib/tailscale/tailscaled.state --socket=/var/run/tailscale/tailscaled.sock --tun=userspace-networking &
+    tailscaled --state=/var/lib/tailscale/tailscaled.state --socket=/var/run/tailscale/tailscaled.sock --tun=userspace-networking --outbound-http-proxy-listen=localhost:1055 --socks5-server=localhost:1055 &
     sleep 2
     tailscale up --authkey="$TAILSCALE_AUTHKEY" --hostname="budget-bot"
     echo "Waiting for Tailscale to connect..."
